ad418016cc3a006f239f35de75ddd10db71608da
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2023-01-17 15:37:52 +0300

    create login page email and password box

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,14 +8,34 @@ import image from "../assets/result.svg";
 import { Link, useNavigate } from "react-router-dom";
 
 import { useSelector } from "react-redux";
-import { Formik } from "formik";
-import { Fragment } from "react";
+import { Formik, Form } from "formik";
+// import { Fragment } from "react";
+import { TextField } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import * as yup from "yup";
 
-const loginSchema = {};
+const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter valid email")
+    .required("Please enter an email"),
+  password: yup
+    .string()
+    .required("Please enter a password")
+    .min(8, "Password must be min 8 chars")
+    .max(16, "Password must be max 16 chars")
+    .matches(/\d+/, "Password must have a number")
+    .matches(/[a-z]+/, "Passwords must have a lowercase")
+    .matches(/[A-Z]+/, "Passwords must have a uppercase")
+    .matches(/[!,?{}><%&$#£+-.]+/, "Passwords must have e special characters"),
+  // age: yup.number().required().positive().integer(),
+  // email: yup.string().email(),
+  // website: yup.string().ur1(),
+});
 
 const Login = () => {
   const navigate = useNavigate();
-  const { currentUser, error } = useSelector((state) => state?.auth);
+  const { currentUser, error, loading } = useSelector((state) => state?.auth);
 
   return (
     <Container maxWidth="lg">
@@ -63,7 +83,53 @@ const Login = () => {
               actions.resetForm();
               actions.setSubmitting(false);
             }}
-          ></Formik>
+          >
+            {({
+              values,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              touched,
+              errors,
+            }) => (
+              <Form>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                  <TextField
+                    label="Email"
+                    name="email"
+                    id="email"
+                    type="email"
+                    variant="outlined"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                  />
+                  <TextField
+                    label="Password"
+                    name="password"
+                    id="password"
+                    type="password"
+                    variant="outlined"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                  />
+                  <LoadingButton
+                    type="submitx"
+                    loading={loading}
+                    loadingPosition="center"
+                    variant="contained"
+                  >
+                    Submit
+                  </LoadingButton>
+                </Box>
+              </Form>
+            )}
+          </Formik>
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/register">Do you have not an account?</Link>
